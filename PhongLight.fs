9f0b685376ac5a2c7f92da03f83b3d0bfad6316c
Chițu Ioan <131875575+ionut200328@users.noreply.github.com>
2023-12-02 11:53:43 +0200

    Pregătirea ferestrei și locului de muncă

---
+++ PhongLight.fs
@@ -0,0 +1,44 @@
+﻿#version 330 core
+out vec4 FragColor;
+
+in vec3 Normal;  
+in vec3 FragPos;  
+  
+uniform vec3 lightPos; 
+uniform vec3 viewPos; 
+uniform vec3 lightColor;
+uniform vec3 objectColor;
+
+// Adăugați o variabilă uniformă pentru constanta de reflexie ambientală
+uniform float ambientReflection;
+// Adăugați o variabilă uniformă pentru constanta de reflexie difuză
+uniform float diffuseReflection;
+// Adăugați variabile uniforme pentru reflexia speculară
+uniform float specularReflection;
+// Adăugați o variabilă uniformă pentru exponentul specular
+uniform int specularExponent;
+
+
+void main()
+{
+    // Calculați iluminarea ambientală
+    vec3 ambient = ambientReflection * lightColor;
+
+    // Calculați difuzia
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * diffuseReflection * lightColor;
+
+    // Calculați iluminarea speculară
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), specularExponent);
+    vec3 specular = specularReflection * spec * lightColor;
+
+    // Calculați culoarea finală utilizând componentele ambientale, difuze și speculară
+    vec3 result = (ambient + diffuse + specular) * objectColor;
+
+    FragColor = vec4(result, 1.0);
+}
+
