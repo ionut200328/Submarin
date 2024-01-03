3ddb1cc66857207ec2679b8e31fa0496ff6b65b9
Csere Cosmin <cserecosmin@yahoo.com>
2024-01-03 14:40:50 +0200

    Textures have been finally loaded onto the submarine

--- Shaders/PhongLight.fs
+++ Shaders/PhongLight.fs
@@ -14,16 +14,21 @@ uniform sampler2D texture5;
 
 // Flag to enable/disable texture
 uniform bool useTexture;  
+vec4 textureColor1;
+vec4 textureColor2; 
+vec4 textureColor3;
+vec4 textureColor4;
+vec4 textureColor5;
 
 void main()
 {
     // If using textures, sample the textures and apply them to FragColor
     if (useTexture) {
-        vec4 textureColor1 = texture(texture1, TexCoord);
-        vec4 textureColor2 = texture(texture2, TexCoord);
-        vec4 textureColor3 = texture(texture3, TexCoord);
-        vec4 textureColor4 = texture(texture4, TexCoord);
-        vec4 textureColor5 = texture(texture5, TexCoord);
+        textureColor1 = texture(texture1, TexCoord);
+        textureColor2 = texture(texture2, TexCoord);
+        textureColor3 = texture(texture3, TexCoord);
+        textureColor4 = texture(texture4, TexCoord);
+        textureColor5 = texture(texture5, TexCoord);
 
         FragColor = textureColor1 * textureColor2 * textureColor3 * textureColor4 * textureColor5;
     } else {
